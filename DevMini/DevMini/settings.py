a1648cc9bd9fc0b456a72b0886a3198ac88cc65f
Nguyễn Cao Kỳ <Caoky.nguyen.1704@gmail.com>
2020-04-27 01:20:13 +0700

    deloy that bai lan 1

--- DevMini/DevMini/settings.py
+++ DevMini/DevMini/settings.py
@@ -48,9 +48,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'DevMini.urls'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
