a7203f33baadd72c5beac3ea0a75129f6395b436
Nguyễn Cao Kỳ <Caoky.nguyen.1704@gmail.com>
2020-04-27 13:14:49 +0700

    okk

--- DevMini/DevMini/settings.py
+++ DevMini/DevMini/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,10 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = [   os.path.join(BASE_DIR, "static"),]
 AUTH_USER_MODEL = 'home.MyUser'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
